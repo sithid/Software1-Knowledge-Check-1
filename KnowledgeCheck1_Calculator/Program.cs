d93c00d99e17faaa68516d7d4f7e6f42cd9470d9
sithid <demonic_urges05@hotmail.com>
2024-08-08 23:36:47 -0400

    update program.cs
    Formatting updates.

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,43 +22,66 @@ namespace KnowledgeCheck1_Calculator
         {
             // I do not like the original menu.
 
-            Console.WriteLine("Hello! Select your math operation!");
-            Console.WriteLine("1.Addition\n\r" +
-                              "2.Subtraction\n\r" +
-                              "3.Multiplication\n\r" +
-                              "4.Division");
+            Console.WriteLine("Hello and welcome to the KnowledgeCheck1 Calculator! ");
+            Console.WriteLine("Select your math operation!");
+
+            Console.WriteLine("1. Addition\n\r" +
+                              "2. Subtraction\n\r" +
+                              "3. Multiplication\n\r" +
+                              "4. Division\n\r" +
+                              "9. Quit");
 
             var input = Console.ReadLine();
             var calculator = new Calculator();
 
-            switch (input)
+            Console.WriteLine();
+
+            do
             {
-                case "1":
-                    {
-                        ProcessSelection(calculator, Operation.Add);
-                        break;
-                    }
-                case "2":
-                    {
-                        ProcessSelection(calculator, Operation.Subtract);
-                        break;
-                    }
-                case "3":
-                    {
-                        ProcessSelection(calculator, Operation.Multiply);
-                        break;
-                    }
-                case "4":
-                    {
-                        ProcessSelection(calculator, Operation.Divide);
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Unknown Selection");
-                        break;
-                    }
+                switch (input)
+                {
+                    case "1":
+                        {
+                            ProcessSelection(calculator, Operation.Add);
+                            break;
+                        }
+                    case "2":
+                        {
+                            ProcessSelection(calculator, Operation.Subtract);
+                            break;
+                        }
+                    case "3":
+                        {
+                            ProcessSelection(calculator, Operation.Multiply);
+                            break;
+                        }
+                    case "4":
+                        {
+                            ProcessSelection(calculator, Operation.Divide);
+                            break;
+                        }
+                    case "9":
+                        {
+                            Console.WriteLine("Goodbye!");
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Unknown Selection");
+                            break;
+                        }
+                }
+
+                Console.WriteLine("Select your math operation!");
+                Console.WriteLine("1. Addition\n\r" +
+                                  "2. Subtraction\n\r" +
+                                  "3. Multiplication\n\r" +
+                                  "4. Division\n\r" +
+                                  "9. Quit");
+
+                input = Console.ReadLine();
             }
+            while (!string.Equals(input, "9"));
         }
 
         private static void ProcessSelection( Calculator calc, Operation operand )
@@ -68,32 +92,34 @@ namespace KnowledgeCheck1_Calculator
             Console.Write("Please enter your second number: ");
             var number2 = Console.ReadLine();
 
-            if( double.TryParse(number1, out double numberOne) && double.TryParse(number2, out double numberTwo))
+            Console.WriteLine();
+
+            if ( double.TryParse(number1, out double numberOne) && double.TryParse(number2, out double numberTwo))
             {
                 switch (operand)
                 {
                     case Operation.Add:
                     {
                         Console.Write($"{numberOne} + {numberTwo} = ");
-                        Console.Write(calc.Add(numberOne, numberTwo));
+                        Console.WriteLine(calc.Add(numberOne, numberTwo));
                         break;
                     }
                     case Operation.Subtract:
                     {
                         Console.Write($"{numberOne} - {numberTwo} = ");
-                        Console.Write(calc.Subtract(numberOne, numberTwo));
+                        Console.WriteLine(calc.Subtract(numberOne, numberTwo));
                         break;
                     }
                     case Operation.Multiply:
                     {
                         Console.Write($"{numberOne} * {numberTwo} = ");
-                        Console.Write(calc.Multiply(numberOne, numberTwo));
+                        Console.WriteLine(calc.Multiply(numberOne, numberTwo));
                         break;
                     }
                     case Operation.Divide:
                     {
                         Console.Write($"{numberOne} / {numberTwo} = ");
-                        Console.Write(calc.Divide(numberOne, numberTwo));
+                        Console.WriteLine(calc.Divide(numberOne, numberTwo));
                         break;
                     }
                     default:
@@ -107,6 +133,8 @@ namespace KnowledgeCheck1_Calculator
             {
                 Console.WriteLine("One or more numbers are not of valid input.");
             }
+
+            Console.WriteLine();
         }
     }
 }
